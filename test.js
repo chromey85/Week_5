518f86b3077bfca2ba26784986f532865c5e4b9b
chromey85 <yayyub85@gmail.com>
2022-02-08 23:38:08 +0000

    fixed images

---
+++ test.js
@@ -0,0 +1,7 @@
+n = 2
+while (n < 21) {
+    if ((1 == (n / n) && (n == (n / 1))) || (n / (Math.random() * 20) != n)) {
+    console.log(`Yes, ${n} is a Prime Number`);}
+     else {(`No, ${n} is not a Prime Number`)}
+        n++;
+};
